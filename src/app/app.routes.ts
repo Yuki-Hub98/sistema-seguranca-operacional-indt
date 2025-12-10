fe8bb8ccbd733e85035743f7ad3cdca9b1eadbd8
gabrielsilper <gabrielsilper.main@gmail.com>
2025-12-10 19:54:25 -0400

    feat: inicia criação do componente de header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Funcionarios } from './pages/funcionarios/funcionarios';
+import { Perfil } from './pages/perfil/perfil';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
   { path: 'funcionarios', component: Funcionarios },
+  { path: 'perfil', component: Perfil },
 ];
