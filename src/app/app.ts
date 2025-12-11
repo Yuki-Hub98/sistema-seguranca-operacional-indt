eab31ac4a9b6e16250e7f2f6c6e43afbb08d89a2
gabrielsilper <gabrielsilper.main@gmail.com>
2025-12-10 21:08:20 -0400

    instala fa-icons no projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./core/components/header/header";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('sistema-seguranca-operacional-indt');
+  protected readonly faUser = faUser;
 }
