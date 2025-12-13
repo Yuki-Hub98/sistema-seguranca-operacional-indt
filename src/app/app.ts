4fd1a5300b64df08a92349cf3670bf86aaccf87e
gabrielsilper <gabrielsilper.main@gmail.com>
2025-12-13 12:40:28 -0400

    refactor: instala fa-icons e usa ícones no navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./core/components/header/header";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, FontAwesomeModule],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('sistema-seguranca-operacional-indt');
-  protected readonly faUser = faUser;
 }
