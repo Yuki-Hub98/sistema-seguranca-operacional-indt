e8b801b50dc65cbf18e528a6a9e67b6d823f8490
gabrielsilper <gabrielsilper.main@gmail.com>
2025-12-14 00:07:16 -0400

    wip: ínicio do formulário de perfil

--- src/app/pages/perfil/perfil.ts
+++ src/app/pages/perfil/perfil.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css',
 })
 export class Perfil {
-
+  readonly faUser = faUser;
 }
