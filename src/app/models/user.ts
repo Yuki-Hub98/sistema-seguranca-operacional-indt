872270469f3dbbe17c9564dff9a622e03fd801f1
gabrielsilper <gabrielsilper.main@gmail.com>
2025-12-19 21:47:43 -0400

    feat: binda campos dos form de perfil e adiciona validadores

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,19 +1,31 @@
+import { FormControl } from '@angular/forms';
+
 export interface User {
-    id: number;
-    username: string;
-    email?: string;
-    password: string;
-    firstName: string;
-    lastName?: string;
-    roles: UserRole;
-    isActive: boolean;
+  id: number;
+  username: string;
+  email?: string;
+  password: string;
+  firstName: string;
+  lastName?: string;
+  roles: UserRole;
+  isActive: boolean;
 }
 
-
 export enum UserRole {
-    ADMIN = 'ADMIN',
-    OPERADOR = 'OPERADOR',
-    SUPERVISOR = 'SUPERVISOR',
-    VISITANTE = 'VISITANTE',
-    GESTOR = 'GESTOR'
+  ADMIN = 'ADMIN',
+  OPERADOR = 'OPERADOR',
+  SUPERVISOR = 'SUPERVISOR',
+  VISITANTE = 'VISITANTE',
+  GESTOR = 'GESTOR',
+}
+
+export interface UserFormGroup {
+  username: FormControl<string | undefined | null>;
+  email: FormControl<string | undefined | null>;
+  newPassword: FormControl<string | undefined | null>;
+  confirmPassword: FormControl<string | undefined | null>;
+  firstName: FormControl<string | undefined | null>;
+  lastName: FormControl<string | undefined | null>;
+  roles: FormControl<UserRole | undefined | null>;
+  isActive: FormControl<boolean | undefined | null>;
 }
--- src/app/pages/perfil/perfil.ts
+++ src/app/pages/perfil/perfil.ts
@@ -3,10 +3,13 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faEnvelope, faUser, faIdBadge } from '@fortawesome/free-regular-svg-icons';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from '../../core/services/auth';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserFormGroup, UserRole } from '../../models/user';
+import { senhaIgualValidator } from '../../core/validators/senha-igual-validator';
 
 @Component({
   selector: 'app-perfil',
-  imports: [FaIconComponent],
+  imports: [FaIconComponent, ReactiveFormsModule],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css',
 })
@@ -15,8 +18,35 @@ export class Perfil {
   readonly faEnvelope = faEnvelope;
   readonly faLock = faLock;
   readonly faIdBadge = faIdBadge;
+  readonly rolesOptions = Object.values(UserRole);
+  readonly adminRole = UserRole.ADMIN;
 
   private authService = inject(AuthService);
+  private formBuilder = inject(FormBuilder);
 
-  currentUser = this.authService.currentUser;
+  currentUser = this.authService.currentUser();
+
+  userForm = this.formBuilder.group<UserFormGroup>(
+    {
+      username: new FormControl(this.currentUser?.username, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl(this.currentUser?.email, [Validators.email]),
+      newPassword: new FormControl('', [Validators.minLength(6)]),
+      confirmPassword: new FormControl(''),
+      firstName: new FormControl(this.currentUser?.firstName, [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      lastName: new FormControl(this.currentUser?.lastName, [Validators.minLength(2)]),
+      roles: new FormControl(this.currentUser?.roles, Validators.required),
+      isActive: new FormControl(this.currentUser?.isActive, Validators.required),
+    },
+    { validators: [senhaIgualValidator('newPassword', 'confirmPassword')] }
+  );
+
+  onSubmit() {
+      console.log('Form submitted:', this.userForm.value);
+  }
 }
